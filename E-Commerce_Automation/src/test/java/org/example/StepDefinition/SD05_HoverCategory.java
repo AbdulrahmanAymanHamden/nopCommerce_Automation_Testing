86b24cd5a4e12bc5756a44a1698242d7f15d3c92
abdulrahman <abdo.ayman.ha@gmail.com>
2022-06-15 19:55:29 +0200

    First Commit

---
+++ E-Commerce_Automation/src/test/java/org/example/StepDefinition/SD05_HoverCategory.java
@@ -0,0 +1,40 @@
+package org.example.StepDefinition;
+
+import org.example.Pages.P05_HoverCategory;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+
+public class SD05_HoverCategory {
+
+    P05_HoverCategory Hover_Page = new P05_HoverCategory();
+
+    @When("User Hovers to Computer Category")
+    public void Hover_to_Category() throws InterruptedException {
+        Actions action = new Actions(Hooks.driver);
+        action.moveToElement(Hover_Page.Computer_Category()).perform();
+        Thread.sleep(2000);
+    }
+
+    @And("User Selects Desktops sub-category")
+    public void Select_SubCategory()
+    {
+        String SubCategoryText = Hover_Page.Desktop_SubCategory().getText().toLowerCase().trim();
+
+        Hover_Page.Desktop_SubCategory().click();
+
+        String PageTitle = Hover_Page.Desktop_PageTitle().getText().toLowerCase().trim();
+
+        Assert.assertTrue(PageTitle.contains(SubCategoryText));
+
+    }
+
+    @Then("^User should be directed to sub-category page \"(.*)\"$")
+    public void Direct_subCategory_Page(String Url)
+    {
+        Assert.assertEquals(Hooks.driver.getCurrentUrl() , Url);
+    }
+}
